2951900bb784aa3aafca8f343b11f49619b2a210
Ironman680-0202 <sandeep.chandra52361234@gmail.com>
2026-05-24 12:51:51 +0530

    first commit

---
+++ ec2.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "terraform_ec2" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+ 
+  associate_public_ip_address = true
+ 
+  tags = {
+    Name = "terraform-ec2"
+  }
+}
+ 
